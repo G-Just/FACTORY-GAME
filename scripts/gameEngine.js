2202298233c8be219aec6c5fd630f64db898de6a
Justas Gužė <gjustas000@gmail.com>
2023-08-01 19:52:56 +0300

    New game functionality

--- scripts/gameEngine.js
+++ scripts/gameEngine.js
@@ -12,7 +12,7 @@ class Memory {
   constructor() {
     //Create a grid array with data inside (0 = empty)
     //Height of the array (rows)
-    for (let j = 0; j < 29; j++) {
+    for (let j = 0; j < 30; j++) {
       if (columns.length !== 0) {
         grid.push(columns);
       }
@@ -27,24 +27,17 @@ class Memory {
 }
 
 class Background {
-  constructor() {}
-  draw() {
-    // canvas background color
-    pen.fillStyle = "rgb(221, 255, 201)";
-    pen.fillRect(0, 0, 1600, 896);
-  }
-}
-
-class Grid {
   constructor() {
-    // draw a grid
     this.x = 0;
     this.y = -32;
     this.width = 32;
     this.height = 32;
   }
-  // Draws the grid based on grid array
   draw() {
+    // canvas background color
+    pen.fillStyle = "rgb(221, 255, 201)";
+    pen.fillRect(0, 0, 1600, 896);
+    // canvas grid drawing
     pen.strokeStyle = "black";
     grid.forEach((column) => {
       pen.moveTo(this.x, this.y);
@@ -65,7 +58,7 @@ class Ore {
   }
   generate() {
     this.generatePosition = grid[Math.floor(Math.random() * grid.length)][
-      Math.floor(Math.random() * grid[0].length)
+      Math.floor(Math.random() * grid[0].length + 1)
     ] = 1;
   }
   place() {
@@ -81,11 +74,34 @@ class Ore {
   }
 }
 
+class Building {
+  constructor(type) {
+    this.type = type;
+  }
+  place() {
+    for (let row = 0; row < grid.length + 1; row++) {
+      for (let column = 0; column < grid[row].length; column++) {
+        if (grid[row][column] === 1) {
+          pen.fillStyle = "black";
+          pen.font = "40px consolas";
+          pen.fillText("B", column * 32 + 5, row * 32 - 3);
+          grid[row][column] = 2;
+        }
+      }
+    }
+  }
+}
+
 function animate() {
-  drawGrid.draw();
-  startingIronOre.place();
   window.requestAnimationFrame(animate);
 }
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "1") {
+    new Building("mine").place();
+    console.log("selected [1] mine building");
+  }
+});
 
 //Initialization
 //Memory load in first always
@@ -93,8 +109,8 @@ new Memory();
 
 const background = new Background();
 background.draw();
-const drawGrid = new Grid();
 const startingIronOre = new Ore("iron");
 startingIronOre.generate();
+startingIronOre.place();
 
 animate();
