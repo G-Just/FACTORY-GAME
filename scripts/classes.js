b0114654214f175b4722b60ca408b81fedf76c65
Justas Gužė <gjustas000@gmail.com>
2023-08-03 02:08:02 +0300

    Changing game engine with different execution type

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,1 +1,32 @@
+class Ore {
+  constructor(type) {
+    this.type = type;
+    this.width = 32;
+    this.height = 32;
+    let setY = Math.floor(Math.random() * 28);
+    let setX = Math.floor(Math.random() * 50);
+    //if the random coordinate is taken generate another one
+    while (grid[setY][setX] !== "empty") {
+      setY = Math.floor(Math.random() * 28);
+      setX = Math.floor(Math.random() * 50);
+    }
+    this.position = { setX, setY };
+  }
+  add() {
+    grid[this.position.setY][this.position.setX] = this.type;
+  }
+  destroy() {
+    grid[this.position.setY][this.position.setX] = "empty";
+  }
+}
 
+class Building {
+  constructor(type) {
+    this.type = type;
+    this.width = 32;
+    this.height = 32;
+  }
+  add() {
+    grid[y / this.height][x / this.width] = this.type;
+  }
+}
--- scripts/gameEngine.js
+++ scripts/gameEngine.js
@@ -10,25 +10,26 @@ let columns = new Array();
 
 //Create a grid array with data inside (0 = empty)
 //Height of the array (rows)
-for (let j = 0; j < 30; j++) {
+for (let j = 0; j < 29; j++) {
   if (columns.length !== 0) {
     grid.push(columns);
   }
   columns = [];
   //Width of the array (columns)
   for (let i = 0; i < 50; i++) {
-    columns.push(0);
+    columns.push("empty");
   }
 }
-// after this we have ( array => grid[row][column] )
+// after this we have ( array => grid[column:y][row:x] )
 
+//Function that draws the background and grid
 function background_grid() {
   width = 32;
   height = 32;
   x = 0;
   y = -32;
   // canvas background color
-  pen.fillStyle = "rgb(221, 255, 201)";
+  pen.fillStyle = "rgb(170, 255, 201)";
   pen.fillRect(0, 0, 1600, 896);
   // canvas grid drawing
   pen.strokeStyle = "black";
@@ -44,8 +45,66 @@ function background_grid() {
   });
 }
 
-function animate() {
+// Mouse event variable
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+// Event listener that gets the mouse event variable values
+canvas.addEventListener("mousemove", (mouseMove) => {
+  mouse.x = mouseMove.clientX;
+  mouse.y = mouseMove.clientY;
+});
+
+// function that draws everything on the board based on the grid cell parameters
+function draw() {
+  for (let i = 0; i < grid.length; i++) {
+    const rows = grid[i];
+    for (let j = 0; j < rows.length; j++) {
+      switch (grid[i][j]) {
+        case "empty":
+          break;
+        case "iron":
+          pen.fillStyle = "blue";
+          pen.fillRect(j * 32, i * 32, 32, 32);
+          break;
+        case "mine":
+          pen.fillStyle = "green";
+          pen.fillRect(j * 32, i * 32, 32, 32);
+          break;
+        case "conveyor":
+          pen.fillStyle = "rgb(30,30,30)";
+          pen.fillRect(j * 32, i * 32, 32, 32);
+          break;
+        case "smelter":
+          pen.fillStyle = "yellow";
+          pen.fillRect(j * 32, i * 32, 32, 32);
+          break;
+      }
+    }
+  }
+}
+
+// Initializing staring classes
+const startingOre = new Ore("iron").add();
+
+// Animation loop
+var delta = 20; //delay between frames
+var oldTime = 0;
+function animate(currentTime) {
+  if (oldTime === 0) {
+    oldTime = currentTime;
+  }
+  if (currentTime - oldTime >= delta) {
+    background_grid();
+    draw();
+    oldTime = currentTime;
+  }
   window.requestAnimationFrame(animate);
 }
 
-animate();
+// Starting animation loop
+requestAnimationFrame(animate);
+
+draw();
